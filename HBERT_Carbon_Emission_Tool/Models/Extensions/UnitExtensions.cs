d8f2d8b47a095cda14b1fc24596111569a6d7210
Nicholas Rawitscher <nicholas@bimorph.com>
2021-11-17 08:43:17 -0500

    deleted multiple Annotiations.cs files modified csproj files

--- HBERT_Carbon_Emission_Tool/Models/Extensions/UnitExtensions.cs
+++ HBERT_Carbon_Emission_Tool/Models/Extensions/UnitExtensions.cs
@@ -1,15 +1,34 @@
-﻿using Autodesk.Revit.DB;
+﻿using System.Windows.Controls;
+using Autodesk.Revit.DB;
 
 namespace CarbonEmissionTool.Models
 {
     public static class UnitExtensions
     {
+
+#if USE_FORGETYPEID
+        //var forgeTypeId = new ForgeTypeId(UnitTypeId.SquareMeters.TypeId);
+        //areaMetric = UnitUtils.ConvertFromInternalUnits(areaInternal, forgeTypeId);
+
         /// <summary>
         /// Converts the input value into Revit internal units - decimal feet.
         /// </summary>
-        public static double ToDecimalFeet(this double value, DisplayUnitType unitType = DisplayUnitType.DUT_MILLIMETERS)
+        public static double ToDecimalFeet(this double value)
         {
-            return UnitUtils.ConvertToInternalUnits(value, unitType);
+            var forgeTypeId = new ForgeTypeId(UnitTypeId.Millimeters.TypeId);
+
+            return UnitUtils.ConvertToInternalUnits(value, forgeTypeId);
         }
+#else
+
+        /// <summary>
+        /// Converts the input value into Revit internal units - decimal feet.
+        /// </summary>
+        //public static double ToDecimalFeet(this double value, DisplayUnitType unitType = DisplayUnitType.DUT_MILLIMETERS)
+        //{
+        //    return UnitUtils.ConvertToInternalUnits(value, unitType);
+        //}
+#endif
+
     }
 }
