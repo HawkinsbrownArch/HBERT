cfb0d316db391498f15da22745a764547a1a7721
Ben Robinson <ben_robinson00@hotmail.com>
2021-11-24 11:59:38 +0000

    Revert "Bimorph multi version"

--- HBERT_Carbon_Emission_Tool/Models/Extensions/UnitExtensions.cs
+++ HBERT_Carbon_Emission_Tool/Models/Extensions/UnitExtensions.cs
@@ -1,34 +1,15 @@
-﻿using System.Windows.Controls;
-using Autodesk.Revit.DB;
+﻿using Autodesk.Revit.DB;
 
 namespace CarbonEmissionTool.Models
 {
     public static class UnitExtensions
     {
-
-#if USE_FORGETYPEID
-        //var forgeTypeId = new ForgeTypeId(UnitTypeId.SquareMeters.TypeId);
-        //areaMetric = UnitUtils.ConvertFromInternalUnits(areaInternal, forgeTypeId);
-
         /// <summary>
         /// Converts the input value into Revit internal units - decimal feet.
         /// </summary>
-        public static double ToDecimalFeet(this double value)
+        public static double ToDecimalFeet(this double value, DisplayUnitType unitType = DisplayUnitType.DUT_MILLIMETERS)
         {
-            var forgeTypeId = new ForgeTypeId(UnitTypeId.Millimeters.TypeId);
-
-            return UnitUtils.ConvertToInternalUnits(value, forgeTypeId);
+            return UnitUtils.ConvertToInternalUnits(value, unitType);
         }
-#else
-
-        /// <summary>
-        /// Converts the input value into Revit internal units - decimal feet.
-        /// </summary>
-        //public static double ToDecimalFeet(this double value, DisplayUnitType unitType = DisplayUnitType.DUT_MILLIMETERS)
-        //{
-        //    return UnitUtils.ConvertToInternalUnits(value, unitType);
-        //}
-#endif
-
     }
 }
--- HBERT_Carbon_Emission_Tool/Models/Main/CarbonEmissionToolMain.cs
+++ HBERT_Carbon_Emission_Tool/Models/Main/CarbonEmissionToolMain.cs
@@ -41,7 +41,6 @@ namespace CarbonEmissionTool.Models
                 }
 
                 HelpDialog.HbertSuccessfullyRun();
-
             }
         }
     }
